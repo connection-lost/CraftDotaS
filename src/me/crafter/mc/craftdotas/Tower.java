847127e633e6d5a7a7c55da707f0de4840fec063
connection-lost <frankwu1995@hotmail.com>
2015-12-21 16:06:05 -0600

    Filled tower's local variables

--- src/me/crafter/mc/craftdotas/Tower.java
+++ src/me/crafter/mc/craftdotas/Tower.java
@@ -1,17 +1,46 @@
 package me.crafter.mc.craftdotas;
 
+import org.bukkit.Location;
+
 public class Tower {
 
 	private int side;
 	private int id;
-	private double damage;
 	private double health;
 	private double maxhealth;
+	private double damage;
+	private int attackspeed;
+	private int attackcooldown;
+	private String displayName;
+	private Location[] locations;
+	private boolean invulnerable;
+	private int[] unlocks;
 	
 	
 	
 	
 	
 	
+	
+	
+	
+	
+	
+	
+	public int getSide() {return side;}
+	public int getId() {return id;}
+	public double getHealth() {return health;}
+	public double getgetMaxHealth() {return maxhealth;}
+	public double getdamage() {return damage;}
+	public int getAttackSpeed() {return attackspeed;}
+	public int getAttackCooldown() {return attackcooldown;}
+	public String getDisplayName() {return displayName;}
+	public Location[] getLocations() {return locations;}
+	public boolean isInvulnerable() {return invulnerable;}
+	public int[] getUnlocks() {return unlocks;}
+
+	public void setHealth(double newhealth) {health = newhealth;}
+	public void setAttackCooldown(int newattackcooldown) {attackcooldown = newattackcooldown;}
+	public void setInvulnerable(boolean newinvulnerable) {invulnerable = newinvulnerable;}
 	
 }

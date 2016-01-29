391b72809c12960ec2a39dd94cf53f13b568950b
connection-lost <frankwu1995@hotmail.com>
2016-01-29 14:54:51 -0600

    Added game action enum

---
+++ src/me/crafter/mc/craftdotas/data/GameAction.java
@@ -0,0 +1,15 @@
+package me.crafter.mc.craftdotas.data;
+
+public enum GameAction {
+
+	SYSTEM_ANNOUNCE, // Say message
+	SYSTEM_COMMAND, // Run command
+	BATTLEFIELD_PLAYER_READY, // Teleport players to battlefield
+	BATTLEFIELD_GATE_OPEN, // Open player base gate
+	BATTLEFIELD_GATE_CLOSE, // Close player base gate
+	BATTLEFIELD_SPAWN_NEUTRAL, // Spawn neutral creeps
+	BATTLEFIELD_SPAWN_CREEP, // Spawn teamed creeps
+	SYSTEM_END, // Game force end after a while
+	SYSTEM_STOP; // Game terminate
+	
+}

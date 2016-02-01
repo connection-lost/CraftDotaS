69f0243051015549a6bcd01bcc8ccd3ad6d3832f
connection-lost <frankwu1995@hotmail.com>
2016-02-01 10:01:33 -0600

    Added team config and team class

--- src/me/crafter/mc/craftdotas/object/Team.java
+++ src/me/crafter/mc/craftdotas/object/Team.java
@@ -2,7 +2,20 @@ package me.crafter.mc.craftdotas.object;
 
 public class Team {
 
+	private int id;
+	private String displayname;
+	private String prefix;
+	private String suffix;
 	
+	public Team(int id_, String displayname_, String prefix_, String suffix_){
+		id = id_;
+		displayname = displayname_;
+		prefix = prefix_;
+		suffix = suffix_;
+	}
 	
-	
+	public int getId() {return id;}
+	public String getDisplayName() {return displayname;}
+	public String getPrefix() {return prefix;}
+	public String getSuffix() {return suffix;}
 }

b9d65aeea3dadfe0fc4aad38a0a5cf8f8f5c05bc
connection-lost <frankwu1995@hotmail.com>
2016-02-01 10:11:20 -0600

    Add spawn location

--- src/me/crafter/mc/craftdotas/object/Team.java
+++ src/me/crafter/mc/craftdotas/object/Team.java
@@ -1,21 +1,26 @@
 package me.crafter.mc.craftdotas.object;
 
+import org.bukkit.Location;
+
 public class Team {
 
 	private int id;
 	private String displayname;
 	private String prefix;
 	private String suffix;
+	private Location spawnlocation;
 	
-	public Team(int id_, String displayname_, String prefix_, String suffix_){
+	public Team(int id_, String displayname_, String prefix_, String suffix_, Location spawnlocation_){
 		id = id_;
 		displayname = displayname_;
 		prefix = prefix_;
 		suffix = suffix_;
+		spawnlocation = spawnlocation_;
 	}
 	
 	public int getId() {return id;}
 	public String getDisplayName() {return displayname;}
 	public String getPrefix() {return prefix;}
 	public String getSuffix() {return suffix;}
+	public Location getSpawnLocation() {return spawnlocation;}
 }

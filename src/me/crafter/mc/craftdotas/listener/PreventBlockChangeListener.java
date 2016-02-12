e8e77cfedd522f45dbd81f8b95ab6961e3086061
connection-lost <frankwu1995@hotmail.com>
2016-02-12 16:06:59 -0600

    Added class to prevent player change blocks. Creative players are ignored

---
+++ src/me/crafter/mc/craftdotas/listener/PreventBlockChangeListener.java
@@ -0,0 +1,31 @@
+package me.crafter.mc.craftdotas.listener;
+
+import org.bukkit.GameMode;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+import me.crafter.mc.craftdotas.object.Game;
+
+public class PreventBlockChangeListener {
+
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onPlayerBreakBlock(BlockBreakEvent event){
+		if (event.getBlock().getWorld() == Game.getWorld()){
+			if (!(event.getPlayer().getGameMode() == GameMode.CREATIVE)){
+				event.setCancelled(true);
+			}
+		}
+	}
+	
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onPlayerPlaceBlock(BlockPlaceEvent event){
+		if (event.getBlock().getWorld() == Game.getWorld()){
+			if (!(event.getPlayer().getGameMode() == GameMode.CREATIVE)){
+				event.setCancelled(true);
+			}
+		}
+	}
+	
+}

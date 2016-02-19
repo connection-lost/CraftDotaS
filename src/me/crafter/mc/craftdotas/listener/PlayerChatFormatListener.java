5ebadb69f88df487e07b43132ffc22ba7c59d610
connection-lost <frankwu1995@hotmail.com>
2016-02-19 12:04:54 -0600

    Add team chat

---
+++ src/me/crafter/mc/craftdotas/listener/PlayerChatFormatListener.java
@@ -0,0 +1,23 @@
+package me.crafter.mc.craftdotas.listener;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+import me.crafter.mc.craftdotas.object.Game;
+import me.crafter.mc.craftdotas.object.Team;
+
+public class PlayerChatFormatListener implements Listener{
+
+	@EventHandler(priority = EventPriority.LOW)
+	public void onPlayerChat(AsyncPlayerChatEvent event){
+		Player player = event.getPlayer();
+		if (player.getWorld() == Game.getWorld()){
+			Team team = Team.getPlayerTeam(player);
+			event.setFormat(team.getPrefix() + "%s" + team.getSuffix() + ": %s");
+		}
+	}
+	
+}
